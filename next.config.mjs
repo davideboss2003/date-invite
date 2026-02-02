fe3ad809f6411fc5a44befc4d6b639975ff5c935
davideboss2003 <muresantiberiu55@yahoo.com>
2026-02-02 23:25:58 +0200

    Add smart basePath for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const repo = 'date-invite';
+
 const nextConfig = {
   output: "export",
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : '',
   typescript: {
     ignoreBuildErrors: true,
   },
